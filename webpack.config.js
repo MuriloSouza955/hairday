74091a2c149ee8383781448919a6391cb56b547b
MuriloSouza955 <souza.murilo@gmail.com>
2025-06-07 19:24:37 -0300

    Carregando o HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    target: 'web',
-    mode: 'development',
-    entry : path.resolve(__dirname, 'src', 'main.js'),
-    output: {
-        filename: "mainModule.js",
-        path: path.resolve(__dirname, 'dist')
+  target: 'web',
+  mode: 'development',
+  entry : path.resolve(__dirname, 'src', 'main.js'),
+  output: {
+      filename: "mainModule.js",
+      path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'dist'),
-      },
-      port: 3000,
-      open: true,
-      liveReload: true,
-    },
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
+  plugins: [
+      new htmlWebpackPlugin({
+          template: path.resolve(__dirname, 'index.html'),
+      })
+  ],
 }
